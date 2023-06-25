f8b419a1ab62f9e8f332daf33e32760ab11f8108
igu1 <eesaard@gmail.com>
2023-06-25 20:20:15 +0530

    Members Added

--- library/models.py
+++ library/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
