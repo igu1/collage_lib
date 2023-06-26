482872a265b2c95778659c17e8b1eff3cb26c5cd
igu1 <eesaard@gmail.com>
2023-06-26 11:06:23 +0530

    Some Changes

--- library/urls.py
+++ library/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
