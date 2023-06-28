c7411f3a17ebc6d3a6452ad90b009c1433fcb0db
igu1 <eesaard@gmail.com>
2023-06-28 12:02:21 +0530

    DB changed

--- core_library/settings.py
+++ core_library/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,17 +78,23 @@ WSGI_APPLICATION = 'core_library.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'djongo',
-            'NAME': 'cluster0',
-            'ENFORCE_SCHEMA': False,
-            'CLIENT': {
-                'host': 'mongodb+srv://eesa:eesahello123@cluster0.jfhdgus.mongodb.net/?retryWrites=true&w=majority'
-            }  
-        }
-}
+# DATABASES = {
+#         'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'college_library', 
+#         'USER': 'eesa',
+#         'PASSWORD': 'eesa',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#         }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -131,33 +137,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "SS Library Admin",
-
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_header": "SS Library",
-
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "SS Library",
-
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/logo.png",
-
-    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
-
-    # Logo to use for login form in dark themes (defaults to login_logo)
     "login_logo_dark": None,
-
-    # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
-
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": None,
-
     "welcome_sign": "Welcome to the Ss College library",
-
-    # Copyright on the footer
     "copyright": "SS College Library Ltd",
 "show_ui_builder": False,
 }
